2147f2862c42689c4d918e8d6974b0e93b6b7c51
liuxian <2929504046@qq.com>
2023-07-19 14:20:50 +0800

    开始where

--- table.go
+++ table.go
@@ -1,15 +1,34 @@
 package xianorm
 
-// 设置表名
+// Table 设置表名
 func (d *DB) Table(name string) *DB {
 	d.TableName = name
 
-	//重置引擎
-	// d.resetSmallormEngine()
+	//重置DB
+	d.resetDB()
 	return d
 }
 
-// 获取表名
+// 重置DB
+func (d *DB) resetDB() {
+	d.Prepare = ""
+	d.AllExec = []interface{}{}
+	d.Sql = ""
+	d.WhereParam = ""
+	d.LimitParam = ""
+	d.OrderParam = ""
+	d.OrWhereParam = ""
+	d.WhereExec = []interface{}{}
+	d.UpdateParam = ""
+	d.UpdateExec = []interface{}{}
+	d.FieldParam = "*"
+	d.TransStatus = 0
+	d.Tx = nil
+	d.GroupParam = ""
+	d.HavingParam = ""
+}
+
+// GetTable 获取表名
 func (d *DB) GetTable() string {
 	return d.TableName
 }
---
+++ test/insert_test.go
@@ -0,0 +1,50 @@
+package test
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"xianorm"
+)
+
+type Penson struct {
+	ID   int `xianorm:auto_increment`
+	Name string
+	Age  int
+}
+
+func TestInsert(t *testing.T) {
+	xdb, err := xianorm.NewDB("root", "root", "43.139.195.17:3301", "orm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := Penson{
+		Name: "xxz",
+		Age:  14,
+	}
+	_, err = xdb.Table("person").Insert(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(xdb.Prepare)
+}
+func TestIBatchInsert(t *testing.T) {
+	xdb, err := xianorm.NewDB("root", "root", "43.139.195.17:3301", "orm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ps := make([]Penson, 0)
+	ps = append(ps, Penson{
+		Name: "oop",
+		Age:  22,
+	})
+	ps = append(ps, Penson{
+		Name: "xxz",
+		Age:  32,
+	})
+	_, err = xdb.Table("person").Insert(ps)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(xdb.Prepare)
+}
